38997c3e599660b3aadd04010ab3463c3e279b53
Isrealsix's Macbook pro <isrealsix@icloud.com>
2022-03-26 12:27:53 +0700

    added styles and installed react-beautiful-dnd

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,5 @@
 import { ITask } from "../model";
-import './styles.css';
+import "./styles.css";
 import Task from "./Task";
 
 interface IProps {
@@ -9,18 +9,21 @@ interface IProps {
 
 const TaskList: React.FC<IProps> = ({ tasks, setTasks }) => {
   return (
-    <div className="tasks">
-      {tasks.map(task => (
-        <Task
-          key={task.id}
-          task={task}
-          tasks={tasks}
-          setTasks={setTasks}
-
-        />
-      ))}
+    <div className="container">
+      <div className="tasks">
+        <span className="tasks__heading">Active Tasks</span>
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        ))}
+      </div>
+      <div className="tasks remove">
+        <span className="tasks__heading">Active Tasks</span>
+        {tasks.map((task) => (
+          <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
